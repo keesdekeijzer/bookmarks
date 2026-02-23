c9207b8d2466cf199868c382928fd08e7ab7a865
keesdekeijzer <git_kdk@xpmail.nl>
2026-02-23 20:58:24 +0100

    Implement user login functionality with form validation and templates

--- account/views.py
+++ account/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+from django.http import HttpResponse
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("Logged in successfully!")
+                else:
+                    return HttpResponse("Account is not active.")
+            else:
+                return HttpResponse("Invalid credentials.")
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+ 
+
+
