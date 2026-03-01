b300a998965cd87d9797f1d3725a2b07df2c41fa
keesdekeijzer <git_kdk@xpmail.nl>
2026-03-01 13:40:17 +0100

    Add message display functionality for user profile updates

--- account/views.py
+++ account/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
 
 
 @login_required
@@ -15,6 +16,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Your profile has been updated successfully.')
+        else:
+            messages.error(request, 'Error updating your profile. Please check the details and try again.')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
