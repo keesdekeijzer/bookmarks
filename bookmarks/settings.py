3e512629f342c64400b9cd6d5beb4c3da8bceedd
keesdekeijzer <git_kdk@xpmail.nl>
2026-03-07 22:38:31 +0100

    Add like functionality for images with AJAX support and update templates

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'images.apps.ImagesConfig',
+    'easy_thumbnails',
 ]
 
 MIDDLEWARE = [
--- images/views.py
+++ images/views.py
@@ -3,8 +3,28 @@ from .forms import ImageCreateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Image
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 # views.py images app
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
+
 
 @login_required
 def image_create(request):
